09bdf82c60cbeb49f3740d1a122ae12353c8b6a3
djgdskmkcmm@163.com <djgdskmkcmm@163.com>
2018-02-26 17:02:04 +0800

    add support to generate csv log from screenlog.0

---
+++ tools/generate_log_csv.py
@@ -0,0 +1,53 @@
+import os
+import argparse
+import sys
+import json
+from collections import OrderedDict
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description='Generate log csv from screenlog file'
+    )
+    parser.add_argument(
+        '--input',
+        dest='screenlog_file',
+        help='path to screenlog_file',
+        default='screenlog.0',
+        type=str
+    )
+
+    parser.add_argument(
+        '--output',
+        dest='csvlog_file',
+        help='path to csvlog_file',
+        default='log.csv',
+        type=str
+    )
+
+    if len(sys.argv) == 1:
+        print "generate log csv from default: screenlog.0, output log.csv"
+    return parser.parse_args()
+
+
+def generate_csv(screenlog_file, csvlog_file):
+    if not os.path.exists(screenlog_file):
+        print 'screenlog_file does not exist.'
+        return
+    
+    csv_col = None
+    dict_slice = slice(11,-1)
+    with open(csvlog_file, 'w') as out:
+        with open(screenlog_file, 'r') as f:
+            for line in f:
+                if 'json_stats' not in line:
+                    continue
+                iter_dict = json.loads(line.strip('\n')[dict_slice], object_pairs_hook=OrderedDict)
+                if not csv_col:
+                    csv_col = list(iter_dict.keys())
+                    out.write(','.join(csv_col))
+                out.write('\n'+','.join(str(value).replace(',', ' ') for value in iter_dict.values()))
+
+
+if __name__ == '__main__':
+    args = parse_args()
+    generate_csv(args.screenlog_file, args.csvlog_file)
